e56eb5e04bed61c3c11b461b483f37cae7ac5bd7
vspl3sk <vspl3sk@yandex.ru>
2022-08-19 11:08:10 +0300

    Задача на вывод массива m на n, заполненного вещественными числами

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void GetMatrixDouble(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write($"{alignNumber} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+double[,] array = new double[m, n];
+GetMatrixDouble(array);
+PrintMatrix(array);
+Console.WriteLine();
